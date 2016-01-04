adc479263449db4a6d70de0760e22b798c1644bb
Andrew Gaspar <andrew.gaspar@outlook.com>
2016-01-03 18:46:27 -0800

    Updated install script

--- Install.ps1
+++ Install.ps1
@@ -1,2 +1,17 @@
-mkdir ~\Documents\WindowsPowerShell\Modules -ErrorAction SilentlyContinue
-Copy-Item -Recurse $PSScriptRoot\CmdUsageSyntax ~\Documents\WindowsPowerShell\Modules
+$moduleDirectory = "~\Documents\WindowsPowerShell\Modules"
+
+mkdir $moduleDirectory -ErrorAction SilentlyContinue
+
+$existing = Get-Module "$moduleDirectory\CmdUsageSyntax" -ListAvailable
+
+if($existing) {
+    $local = Get-Module .\CmdUsageSyntax -ListAvailable
+    if($local.Version -lt $existing.Version) {
+        Write-Error "Module is already installed"
+        return
+    } else {
+        rmdir -Recurse -Force "$moduleDirectory\CmdUsageSyntax"
+    }
+}
+
+Copy-Item -Recurse $PSScriptRoot\CmdUsageSyntax $moduleDirectory
